fae5f55998267fcc5d01e7ec392464349b276d82
JorisUn <joris.unikas21@gmail.com>
2024-03-22 23:03:27 +0000

    Added dynanic grid element scalling, input of the number of grid elements, grid clear

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
 const container = document.querySelector(".container");
-function createDivs(number){
+const buttonMenu = document.querySelector(".buttonMenu");
+const clearGridButton = document.querySelector(".gridClearButton");
+const gridLengthInput = document.querySelector(".gridLengthInput");
+const gridLengthButton = document.querySelector(".gridLengthButton");
+let mouseClicked = false;
+document.body.onmousedown = () => {
+    mouseClicked=true;
+}
+document.body.onmouseup = () => {
+    mouseClicked=false;
+}
+
+function createGrid(length){
+    number = Math.pow(length, 2);
     for(let i=0;i<number;i++){
         let box = document.createElement("div");
         box.classList.add("drawBox");
-        box.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        box.style.backgroundColor = "rgb(255, 255, 255)";
+        box.style.aspectRatio="1/1";
+        box.style.width = `${container.clientHeight/length}px`;
         hovering = true;
         box.addEventListener("mouseover", () => {
-            if(hovering){
-                box.style.backgroundColor = `rgba(0, 0, 0, 1)`;
-                hovering=false;
-                }
-            let j=100;
-            let interval = setInterval(() => {
-                j--;
-                box.style.backgroundColor = `rgba(0, 0, 0, ${j/100})`;
-                if(j<0) clearInterval(interval);
-                
-            }, 10);
+            if(mouseClicked){
+                box.style.backgroundColor = `rgb(0, 0, 0)`
+            }
         })
         container.appendChild(box);
     }
 }
-createDivs(64);
+function removeGrid(){
+    const boxes = document.querySelectorAll(".drawBox");
+    for(let i=0;i<boxes.length;i++){
+        container.removeChild(boxes[i]);
+    }
+}
+function clearGrid(){
+    const boxes = document.querySelectorAll(".drawBox");
+    for(let i=0;i<boxes.length;i++){
+        boxes[i].style.backgroundColor = "rgb(255, 255, 255)";
+    }
+}
+//clearGridButton.onclick = clearGrid();
+clearGridButton.textContent = "Clear Grid";
+clearGridButton.style.backgroundColor = "red";
+clearGridButton.onclick = () => clearGrid();
+buttonMenu.appendChild(clearGridButton);
+gridLengthButton.onclick = () => {
+    let value = Number(gridLengthInput.value);
+    if(isNaN(value) || value > 64 || value < 1) return;
+    removeGrid();
+    createGrid(value);
+    console.log(value);
+}; 
+buttonMenu.appendChild(gridLengthButton);
+buttonMenu.appendChild(gridLengthInput);
